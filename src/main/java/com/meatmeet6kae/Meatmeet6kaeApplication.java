de613c53350db8e4bc3ed50d8aec3d7e9dc008d9
algudgod <algudgod@gmail.com>
2024-09-09 13:42:34 +0900

    test commit-0909

---
+++ src/main/java/com/meatmeet6kae/Meatmeet6kaeApplication.java
@@ -0,0 +1,13 @@
+package com.meatmeet6kae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meatmeet6kaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meatmeet6kaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meatmeet6kae/meatmeet6kae/Meatmeet6kaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatmeet6kae.meatmeet6kae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Meatmeet6kaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

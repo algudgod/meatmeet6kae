7f243d99bce8a59de472ab3bd1befea497e3027d
algudgod <algudgod@gmail.com>
2024-09-09 13:50:00 +0900

    test commit-0909-1349

--- src/main/java/com/meatmeet6kae/Meatmeet6kaeApplication.java
+++ src/main/java/com/meatmeet6kae/Meatmeet6kaeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Meatmeet6kaeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Meatmeet6kaeApplication.class, args);
 	}

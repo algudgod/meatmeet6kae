aedf096488289372b1811b356a57e3b039c08c13
algudgod <algudgod@gmail.com>
2024-12-05 16:52:37 +0900

    feat: 게시글 작성 시 이미지 첨부 기능 구현 완료
    - 게시글 작성 시 이미지 첨부 기능 구현 (S3 업로드 포함)
    - 게시글 작성 폼에 이미지 첨부 input 필드 추가
    - 이미지 첨부 관련 예외 처리 완료 (IOException 등)
    - 이미지 첨부 개수 3장으로 제한 (클라이언트 및 서버 검증)

--- src/main/java/com/meatmeet6kae/Meatmeet6kaeApplication.java
+++ src/main/java/com/meatmeet6kae/Meatmeet6kaeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Meatmeet6kaeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("한글테스트입니다.");
 		SpringApplication.run(Meatmeet6kaeApplication.class, args);
 	}
 
